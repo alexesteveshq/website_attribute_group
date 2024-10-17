db811db06d6a8e1383ddcb3bc79d15efd06f4a64
alex <alexesteveshq@gmail.com>
2024-10-17 21:26:32 +0200

    [Alex] update keyword

--- website_attribute_group/__manifest__.py
+++ website_attribute_group/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Website Product Attribute Group',
     'summary': 'Groups attributes by groups/category. (website attribute group | attribute category |'
-               ' product attribute category)',
+               ' product attribute category | attribute subcategory)',
     'description': 'Groups attributes by groups/category',
     'version': '17.0.1.0',
     'category': 'Website',
